12414dd546fb84cefa0871ef50f50f37bf331de6
Haydon <haydon1900@gmail.com>
2026-03-03 12:22:17 +0000

    feat: add RigidBody to Floor component

--- components/canvas/Floor.tsx
+++ components/canvas/Floor.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { RigidBody } from '@react-three/rapier';
+
 export function Floor() {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]} receiveShadow>
-      <planeGeometry args={[20, 20]} />
-      <meshStandardMaterial color="#f0f0f0" />
-    </mesh>
+    <RigidBody type="fixed" colliders="hull">
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]} receiveShadow>
+        <planeGeometry args={[20, 20]} />
+        <meshStandardMaterial color="#2d2d44" />
+      </mesh>
+    </RigidBody>
   );
 }
